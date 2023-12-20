20c41ae0b65d73af497b61673681190b65b2cd69
JJerBum <2hanjunbum6@gmail.com>
2023-12-20 14:35:27 +0900

    feat: added entity(history)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gukppap-backend
+
+go 1.21.2
---
+++ internal/core/domain/history.go
@@ -0,0 +1,7 @@
+package main
+
+type History struct {
+	OriginalURL string `json:"orignalURL"`
+	ShortcutURL string `json:"shortcutURL"`
+	Expiration  string `json:"expiration"`
+}
