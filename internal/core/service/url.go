04d8d2cf5f489fb91d98b636e0ef387f157ba458
JJerBum <2hanjunbum6@gmail.com>
2023-12-20 20:42:06 +0900

    feat(impl): URLService implement port.URLService interface

---
+++ internal/core/service/url.go
@@ -0,0 +1,77 @@
+package service
+
+import (
+	"context"
+	"gukppap-backend/internal/core/domain"
+	"gukppap-backend/internal/core/port"
+
+	"github.com/dchest/uniuri"
+)
+
+// implements port.URLService interface
+type URLService struct {
+	repo port.URLRepository
+}
+
+func NewURLService(repo port.URLRepository) *URLService {
+	return &URLService{
+		repo: repo,
+	}
+}
+
+func (us *URLService) CreateURL(ctx context.Context, URL *domain.URL) (*domain.URL, error) {
+	URL.ShortcutURL = uniuri.NewLenChars(6, []byte(URL.OriginalURL))
+
+	resURL, err := us.repo.CreateURL(ctx, URL)
+	if err != nil {
+		return nil, err
+	}
+
+	return resURL, nil
+}
+
+func (us *URLService) GetURLByShortcutURL(ctx context.Context, shortcutURL string) (*domain.URL, error) {
+	resURL, err := us.repo.GetURLByShortcutURL(ctx, shortcutURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return resURL, nil
+}
+
+func (us *URLService) GetURLByOrignalURL(ctx context.Context, originlURL string) (*domain.URL, error) {
+	resURL, err := us.repo.GetURLByShortcutURL(ctx, originlURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return resURL, nil
+}
+
+func (us *URLService) UpdateURL(ctx context.Context, URL domain.URL) (*domain.URL, error) {
+	resURL, err := us.repo.UpdateURL(ctx, URL)
+	if err != nil {
+		return nil, err
+	}
+
+	return resURL, nil
+}
+
+func (us *URLService) DeleteByShortcutURL(ctx context.Context, shortcutURL string) error {
+
+	err := us.repo.DeleteByShortcutURL(ctx, shortcutURL)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (us *URLService) DeleteByOriginalURL(ctx context.Context, originURL string) error {
+	err := us.repo.DeleteByOriginalURL(ctx, originURL)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
