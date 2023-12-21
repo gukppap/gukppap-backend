d0f253fd34006ba9b10703dcf469448bad439010
JJerBum <2hanjunbum6@gmail.com>
2023-12-21 11:13:55 +0900

    refactor: context was removed
    데이터베이스 트랜젝션 처리는 repository에서 구현함으로 context가 필요없어지게 되었음. 그러므로 por의 inteface를 변경함

--- internal/core/port/url.go
+++ internal/core/port/url.go
@@ -1,24 +1,23 @@
 package port
 
 import (
-	"context"
 	"gukppap-backend/internal/core/domain"
 )
 
 type URLService interface {
-	CreateURL(ctx context.Context, URL *domain.URL) (*domain.URL, error)
-	GetURLByShortcutURL(ctx context.Context, shortcutURL string) (*domain.URL, error)
-	GetURLByOrignalURL(ctx context.Context, originlURL string) (*domain.URL, error)
-	UpdateURL(ctx context.Context, URL domain.URL) (*domain.URL, error)
-	DeleteByShortcutURL(ctx context.Context, shortcutURL string) error
-	DeleteByOriginalURL(ctx context.Context, originURL string) error
+	CreateURL(URL *domain.URL) (*domain.URL, error)
+	GetURLByShortcutURL(shortcutURL string) (*domain.URL, error)
+	GetURLByOrignalURL(originlURL string) (*domain.URL, error)
+	UpdateURL(URL domain.URL) (*domain.URL, error)
+	DeleteByShortcutURL(shortcutURL string) error
+	DeleteByOriginalURL(originURL string) error
 }
 
 type URLRepository interface {
-	CreateURL(ctx context.Context, URL *domain.URL) (*domain.URL, error)
-	GetURLByShortcutURL(ctx context.Context, shortcutURL string) (*domain.URL, error)
-	GetURLByOrignalURL(ctx context.Context, originlURL string) (*domain.URL, error)
-	UpdateURL(ctx context.Context, URL domain.URL) (*domain.URL, error)
-	DeleteByShortcutURL(ctx context.Context, shortcutURL string) error
-	DeleteByOriginalURL(ctx context.Context, originURL string) error
+	CreateURL(URL *domain.URL) (*domain.URL, error)
+	GetURLByShortcutURL(shortcutURL string) (*domain.URL, error)
+	GetURLByOrignalURL(originlURL string) (*domain.URL, error)
+	UpdateURL(URL domain.URL) (*domain.URL, error)
+	DeleteByShortcutURL(shortcutURL string) error
+	DeleteByOriginalURL(originURL string) error
 }
