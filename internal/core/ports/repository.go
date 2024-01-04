0771f1ccfa11181f059963b26559aa26718a6d94
JJerBum <2hanjunbum6@gmail.com>
2024-01-05 00:28:49 +0900

    fix: add ctx

--- internal/core/ports/repository.go
+++ internal/core/ports/repository.go
@@ -1,13 +1,17 @@
 package ports
 
-import "github.com/guckppap/gukppap-backend/internal/core/domain"
+import (
+	"context"
+
+	"github.com/guckppap/gukppap-backend/internal/core/domain"
+)
 
 // ShortcutRepository 인터페이스는 Shortcut의 저장소(repository)를 CRUD를 하기 위한 규약입니다.
 // ShortcutRepository는 Driven Actor(본 프로젝트에서는 Mysql이 해당됩니다.)가 구현합니다.
 type ShorcutRepository interface {
-	GetByUrl(url string) (*domain.Shortcut, error)
-	GetByShortcut(shortuct string) (*domain.Shortcut, error)
-	Save(*domain.Shortcut) error
-	Update(*domain.Shortcut) error
-	Delete(url string) error
+	GetByUrl(ctx context.Context, url string) (*domain.Shortcut, error)
+	GetByShortcut(ctx context.Context, shortuct string) (*domain.Shortcut, error)
+	Save(context.Context, *domain.Shortcut) error
+	Update(context.Context, *domain.Shortcut) error
+	Delete(ctx context.Context, url string) error
 }
